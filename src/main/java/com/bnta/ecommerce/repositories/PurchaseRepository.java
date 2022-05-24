190ef389706ee4d1bdeb81ce247071d3b59e1242
Haining Zhen <hainingzhen@outlook.com>
2022-05-24 09:22:23 +0100

    removed broken query for branching

--- src/main/java/com/bnta/ecommerce/repositories/PurchaseRepository.java
+++ src/main/java/com/bnta/ecommerce/repositories/PurchaseRepository.java
@@ -10,19 +10,5 @@ import java.util.List;
 @Repository
 public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
 
-//    @Query(
-//            name = "SELECT " +
-//                    "* " +
-//                    "FROM " +
-//                    "purchase " +
-//                    "WHERE " +
-//                    "purchase_quantity > 0 AND purchase_quantity < 5 " +
-//                    "AND " +
-//                    "purchased_date > '1000-01-01' AND purchased_date < '4000-03-01' " +
-//                    "AND " +
-//                    "customer_id = 2",
-//            nativeQuery = true
-//    )
-//    List<Purchase> searchAll();
 
 }
