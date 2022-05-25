e9474e1170c92400464db7f0ec0fa2dd7b5d027a
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-05-25 14:12:56 +0100

    find purchase by product id corrected

--- src/test/java/com/bnta/ecommerce/controllers/PurchaseControllerTest.java
+++ src/test/java/com/bnta/ecommerce/controllers/PurchaseControllerTest.java
@@ -15,13 +15,27 @@ class PurchaseControllerTest {
     @Autowired
     PurchaseService purchaseService;
 
-   @Test
+    @Test
     @Transactional
     void findOrderandCustomerByPurchaseID() {
-       Purchase purchase1 = purchaseService.findByPurchaseId(6L).get();
+        Purchase purchase1 = purchaseService.findByPurchaseId(6L).get();
 
-      assertAll("Purchase with ID 6 should show a Lexus IS was purchased by customer Galina Spary",
-              ()-> assertEquals("Lexus", purchase1.getProduct().getManufacturer()),
-              ()-> assertEquals("IS", purchase1.getProduct().getModel()),
-              ()->  assertEquals("Galina Spary", purchase1.getCustomer().getName()));
-   }}
+        assertAll("Purchase with ID 6 should show a Lexus IS was purchased by customer Galina Spary",
+                () -> assertEquals("Lexus", purchase1.getProduct().getManufacturer()),
+                () -> assertEquals("IS", purchase1.getProduct().getModel()),
+                () -> assertEquals("Galina Spary", purchase1.getCustomer().getName()));
+    }
+
+    @Test
+    void findPurchaseByProductId() {
+//will only show what is in the 'basket' so those with false for isPurchased question
+        Purchase purchase2 = purchaseService.findByProductCustomerId(1l, 1l).get();
+
+       assertAll("With Product id 1, should return a Lexus IS F purchased by Benita Pipworth",
+               ()-> assertEquals("Lexus", purchase2.getProduct().getManufacturer()),
+                ()-> assertEquals("IS F", purchase2.getProduct().getModel()),
+                ()-> assertEquals("Benita Pipworth", purchase2.getCustomer().getName()));
+
+
+    }}
+
