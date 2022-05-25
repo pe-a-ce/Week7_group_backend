c922fd57969a22c17b0e2a7cb15de90f71b1360e
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-05-25 15:57:26 +0100

    tests for exceptions in products added

--- src/test/java/com/bnta/ecommerce/services/ProductServiceTest.java
+++ src/test/java/com/bnta/ecommerce/services/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package com.bnta.ecommerce.services;
 
 import com.bnta.ecommerce.models.Product;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,5 +31,13 @@ class ProductServiceTest {
                 ()-> assertEquals(17.42, product1.getPrice())
         );
     }
+        @Test
+        void testExpectedException_withFakeManufacter_Model() {
 
-}
+            Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+                productService.searchByManufacturerAndModel("DiamondMake", "Shiny").get(0);
+            });
+
+            Assertions.assertEquals("No cars found!", thrown.getMessage());
+        }
+    }
