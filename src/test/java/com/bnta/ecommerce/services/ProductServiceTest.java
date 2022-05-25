e38713d1e715333a7dc4edf9d9e5d47ecb776a82
Peace Akib <99202770+pe-a-ce@users.noreply.github.com>
2022-05-25 15:28:33 +0100

    product service get all and get by manu and model

---
+++ src/test/java/com/bnta/ecommerce/services/ProductServiceTest.java
@@ -0,0 +1,35 @@
+package com.bnta.ecommerce.services;
+
+import com.bnta.ecommerce.models.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductServiceTest {
+
+    @Autowired
+    ProductService productService;
+
+    @Test
+    void getAllProducts() {
+        assertEquals(1000, productService.getAllProducts().size());
+    }
+
+    @Test
+    void searchByManufacturerAndModel() throws Exception {
+
+        Product product1 = productService.searchByManufacturerAndModel("Mitsubishi", "Eclipse").get(1);
+
+        assertAll("should bring up car details; price and id",
+                ()-> assertEquals("Mitsubishi", product1.getManufacturer()),
+                ()-> assertEquals("Eclipse", product1.getModel()),
+                ()-> assertEquals(654, product1.getId()),
+                ()-> assertEquals(17.42, product1.getPrice())
+        );
+    }
+
+
+    }
